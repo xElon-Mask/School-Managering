61204414a4c0817b78353a933fc9cc417678f8a4
xElon-Mask <xelonmask@protonmail.com>
2022-04-13 12:07:22 +0200

    add Structs and mappings

--- contracts/SchoolManagering.sol
+++ contracts/SchoolManagering.sol
@@ -5,6 +5,21 @@ pragma solidity 0.8.13;
 contract SchoolManagering {
     address owner;
 
+    struct Rating {
+        string subject;
+        uint rating;
+
+    }
+
+    struct Pupil {
+        string firstName;
+        string lastName;
+        uint numberOfRatings;
+        mapping(uint => Rating) ratings;
+    }
+
+    mapping(address => Pupil) pupils;
+
     constructor() {
         owner = msg.sender;
     }
